70a016ac203e19399e4d0e6c8aa8fc53cdbef758
Nimira43 <n.rabone72@aol.com>
2024-12-07 12:54:39 +0000

    For i in the range of 1 to 10000 generate a random number or either 1, 2 or 3 the update the x and y points. If the random number equals 2 then add that on to the instance of sy[i]

--- python/main.py
+++ python/main.py
@@ -7,3 +7,9 @@ sx = np.zeros(N)
 sy = np.zeros(N)
 
 for i in range(1, N):
+  k = np.random.randint(1, 4)
+  sx[i] = sx[i - 1] / 2 + k - 1
+  sy[i] = sy[i - 1] / 2
+  if k == 2:
+    sy[i] += 2
+    
