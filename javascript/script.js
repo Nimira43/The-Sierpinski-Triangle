0a95e0a8f89ef38960652c070939377baad449cf
Nimira43 <n.rabone72@aol.com>
2024-12-07 13:58:24 +0000

    Loop through to generate the points

--- javascript/script.js
+++ javascript/script.js
@@ -5,4 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const N = 10000
   let sx = [0]
   let sy = [0]
+
+  for (let i = 1; i < N; i++) {
+    const k = Math.floor(Math.random() * 3) + 1
+    sx[i] =sx[i - 1] / (k - 1)
+    sy[i] =sy[i - 1]
+    if (k === 2) {
+      sy[i] += 2
+    }  
+  }
 })
+
