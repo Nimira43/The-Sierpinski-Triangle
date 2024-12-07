a2f86b19986628a391def3b9aa55fd13f2bf1aa3
Nimira43 <n.rabone72@aol.com>
2024-12-07 13:54:01 +0000

    Initialise variables

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('sierpinskiCanvas')
   const ctx = canvas.getContext('2d')
+
+  const N = 10000
+  let sx = [0]
+  let sy = [0]
 })
