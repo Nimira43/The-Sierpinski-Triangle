f430a2227018df8dfef648220cf6df2be91c960b
Nimira43 <n.rabone72@aol.com>
2024-12-07 14:06:04 +0000

    Corrected mistakes on line 12

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
   const canvas = document.getElementById('sierpinskiCanvas')
   const ctx = canvas.getContext('2d')
 
@@ -8,11 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   for (let i = 1; i < N; i++) {
     const k = Math.floor(Math.random() * 3) + 1
-    sx[i] =sx[i - 1] / (k - 1)
-    sy[i] =sy[i - 1]
+    sx[i] = sx[i - 1] / (k - 1)
+    sy[i] = sy[i - 1]
     if (k === 2) {
       sy[i] += 2
-    }  
+    }
+    ctx.fillRect(sx[i] * 100, sy[i] * 100, 1, 1)  
   }
 })
 
